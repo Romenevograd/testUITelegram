7a220c019bc78014bcf11e19b446ace878b71409
Rimkevich Sergey <Serjrome@yandex.ru>
2025-02-09 20:31:54 +0300

    Initial Commit

---
+++ Library/LibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryApp.swift
+//  Library
+//
+//  Created by Сергей Римкевич on 09.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct LibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
